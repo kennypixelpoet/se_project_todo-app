8b9c3009d31b2403c68ec287e2c767b781b5592e
Kenny <kkeagan20@gmail.com>
2026-05-29 00:23:17 -0400

    Todo App Project 8 Sub1

---
+++ components/Popup.js
@@ -0,0 +1,35 @@
+export default class Popup {
+    constructor(popupSelector) {
+        this._popupElement = document.querySelector(popupSelector); 
+    }
+
+    open() {
+        this._popupElement.classList.add("popup_visible");
+        document.addEventListener("keydown", this._handleEscapeClose);
+    }
+
+    close() {
+        this._popupElement.classList.remove("popup_visible");
+        document.removeEventListener("keydown", this._handleEscapeClose);
+    }
+
+    _handleEscapeClose = (evt) => {
+        if (evt.key === "Escape") {
+            this.close();
+        }
+    };
+    
+    setEventListeners() {
+        const closeButton = this._popupElement.querySelector(".popup__close");
+        
+        closeButton.addEventListener("click", () => {
+            this.close();
+        });
+
+    this._popupElement.addEventListener("click", (evt) => {
+    if (evt.target === this._popupElement) {
+        this.close();
+      }
+    });
+  }
+}
---
+++ components/PopupWithForm.js
@@ -0,0 +1,29 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super();
+
+    this._handleFormSubmit = 
+    this._form = 
+    this._inputList = 
+  }
+
+  _getInputValues() {
+    const inputValues = {};
+
+    this._inputList.forEach((input) => {
+      
+    });
+
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._form.addEventListener("submit", (evt) => {
+      
+    });
+  }
+}
